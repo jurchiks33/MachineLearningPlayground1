fe06b4e229d316b2749a18b096528bd7103b4bdd
jurchiks33 <jurisslisans@gmail.com>
2024-04-11 10:10:46 +0200

    working on a  housing data

---
+++ housingPrices.py
@@ -0,0 +1,8 @@
+import numpy as np
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import AdaBoostRegressor
+from sklearn import datasets
+from sklearn.metrics import mean_squared_error, explained_variance_score
+from sklearn.utils import shuffle
+import matplotlib.pyplot as plt
+
