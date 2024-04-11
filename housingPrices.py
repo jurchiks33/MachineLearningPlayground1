ac8fa8f1db8210d0facb64d9d008efa50965402c
jurchiks33 <jurisslisans@gmail.com>
2024-04-11 10:21:39 +0200

    working on a tree

--- housingPrices.py
+++ housingPrices.py
@@ -6,3 +6,21 @@ from sklearn.metrics import mean_squared_error, explained_variance_score
 from sklearn.utils import shuffle
 import matplotlib.pyplot as plt
 
+housing_data = datasets.load_boston()
+
+X, y = shuffle(housing_data.data, housing_data.target, random_state=7)
+
+num_training = int(0.8 * len(X))
+X_train, y_train = X[:num_training], y[:num_training]
+X_test, y_test = X[num_training:], y[num_training:]
+
+#Tree with maximum depth of 4
+dt_regressor = DecisionTreeRegressor(max_depth=4)
+dt_regressor.fit(X_train, y_train)
+
+#Using AdaBoost for decision tree regression model
+ab_regressor = AdaBoostRegressor(DecisionTreeRegressor(max_depth=4),
+                                 n_estimators=400, random_state=7)
+ab_regressor.fit(X_train, y_train)
+
+#Performance evaluation
